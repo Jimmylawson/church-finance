544a9c4d45524c611a6614029650c90d59f39590
jimmy <jimoseipeace2bb@gmail.com>
2026-04-20 22:45:52 -0500

    first commit

---
+++ backend/src/main/java/com/jimmyproject/churchfinancebackend/ChurchFinanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jimmyproject.churchfinancebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchFinanceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchFinanceBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/jimmyproject/churchfinancebackend/ChurchFinanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimmyproject.churchfinancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchFinanceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
